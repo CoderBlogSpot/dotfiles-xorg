7e3bfb437b4e26403a2046498f3fc8432b11c801
CoderBlogSpot <117447826+CoderBlogSpot@users.noreply.github.com>
2023-07-24 13:01:02 +0200

    Update powermenu1.sh

--- .local/bin/powermenu1.sh
+++ .local/bin/powermenu1.sh
@@ -4,7 +4,7 @@ options=" Shutdown\n Reboot\n Logout\n Lock"
 selected=$(echo -e "$options" | rofi -dmenu -i -p "Powermenu" -font "Fira Code Nerd Font 10" -lines 4 -width 20 -padding 20)
 case "$selected" in
         " Shutdown")
-                pkexec /sbin/poweroff -P now
+                pkexec /sbin/poweroff now
                 ;;
         " Reboot")
                 pkexec /sbin/reboot
